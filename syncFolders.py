aa4b6c8b1b05917c21f16b5862669aabfa8cda2f
olexandrit <itvisa1c@gmail.com>
2018-09-17 10:55:43 +0300

    rrrr

--- syncFolders.py
+++ syncFolders.py
@@ -14,8 +14,8 @@ try:
         f1 = lFolders[0]
         f2 = lFolders[1]
 
-        nf1 = re.sub(r"[u'\\']", "", f1)
-        nf2 = re.sub(r"[u'\\'u'\n']", "", f2)
+        nf1 = re.sub(r"[u'\']", "", f1)
+        nf2 = re.sub(r"[u'\'u'\n']", "", f2)
 
         sync(nf1, nf2, 'sync', purge = False)
 
