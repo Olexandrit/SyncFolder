55937827ea0f23392f479ed541049df3b69e15ed
olexandrit <itvisa1c@gmail.com>
2018-09-18 08:53:18 +0300

    rrrr

--- syncFolders.py
+++ syncFolders.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
 
 from dirsync import sync
 import os,re,sys,traceback
+from datetime import datetime
 
 path_def = os.getcwd()
+flogs = open(path_def + '/errors', 'w')
 
 try:
-    nfile = open(path_def+'/settings', 'rt')
+    nfile = open(path_def+'/settings', 'r', encoding="utf-8")
     ldata = nfile.readlines()
 
     for i in ldata:
@@ -17,11 +18,15 @@ try:
         nf1 = re.sub(r"[u'\']", "", f1)
         nf2 = re.sub(r"[u'\'u'\n']", "", f2)
 
+        flogs.write("Run script " + datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S") + "\n")
         sync(nf1, nf2, 'sync', purge = False)
 
     nfile.close()
 
+    flogs.write("Complete script " + datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S") + "\n")
+    flogs.close()
+
 except Exception:
-    flogs = open(path_def + '/errors', 'w')
-    flogs.write("File not found or path is incorrect from " + path_def + "\n"+traceback.format_exc())
+
+    flogs.write("Errors: " + path_def + "\n"+traceback.format_exc())
     flogs.close()
